fa149fd47ea6f8deff6bb09987ee93de90364df8
bain66888 <bain66888@gmail.com>
2026-05-20 20:10:23 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -9,11 +9,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# 2. 【核心优化】强行清除源码自带的同名旧包，防止编译时“双胞胎”冲突报错
-rm -rf package/feeds/luci/luci-app-openclash
-rm -rf package/feeds/packages/luci-app-openclash
-rm -rf package/feeds/luci/luci-app-adguardhome
-rm -rf package/feeds/packages/adguardhome
-
-# 3. 如果需要，你还可以顺手把官方自带的、可能冲突的依赖也清理一下
-rm -rf package/feeds/packages/openclash
+chmod -R 755 ./package/luci-app-adguardhome/*
