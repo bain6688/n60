71b8d6ab8e25b6640bff014007ac5f8472a288f7
bain66888 <bain66888@gmail.com>
2026-05-20 20:18:22 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -8,5 +8,3 @@
 # 1. 添加第三方满血插件源（这一步你写得完全正确）
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-
-chmod -R 755 ./package/luci-app-adguardhome/*
